1670752908e723c01cc9d2a5e9f8483f3d28312a
Promise Fru <info@promisefru.com>
2025-08-14 14:34:07 +0100

    feat: implement Queuedroid SMS sending functionality.

--- sms_outbound.py
+++ sms_outbound.py
@@ -4,6 +4,9 @@ of the GNU General Public License, v. 3.0. If a copy of the GNU General
 Public License was not distributed with this file, see <https://www.gnu.org/licenses/>.
 """
 
+import requests
+import phonenumbers
+from phonenumbers import geocoder
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 from utils import get_configs
@@ -13,6 +16,13 @@ TWILIO_ACCOUNT_SID = get_configs("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = get_configs("TWILIO_AUTH_TOKEN")
 TWILIO_SERVICE_SID = get_configs("TWILIO_SERVICE_SID")
 TWILIO_PHONE_NUMBER = get_configs("TWILIO_PHONE_NUMBER")
+
+QUEUEDROID_API_URL = get_configs(
+    "QUEUEDROID_API_URL", default_value="https://api.queuedroid.com/v1/messages/send"
+)
+QUEUEDROID_API_KEY = get_configs("QUEUEDROID_API_KEY")
+QUEUEDROID_EXCHANGE_ID = get_configs("QUEUEDROID_EXCHANGE_ID")
+QUEUEDROID_SUPPORTED_REGION_CODES = get_configs("QUEUEDROID_SUPPORTED_REGION_CODES")
 
 logger = get_logger(__name__)
 
@@ -37,7 +47,7 @@ def send_with_twilio(phone_number: str, message: str) -> bool:
         status = message_response.status
 
         if status in ("accepted", "pending", "queued"):
-            logger.info("Message sent successfully.")
+            logger.info("Message sent successfully via Twilio.")
             return True
 
         logger.error("Failed to send message. Twilio status: %s", status)
@@ -46,3 +56,49 @@ def send_with_twilio(phone_number: str, message: str) -> bool:
     except TwilioRestException as exc:
         logger.error("Error sending message with Twilio: %s", exc)
         return False
+
+
+def send_with_queuedroid(phone_number: str, message: str) -> bool:
+    """
+    Sends a message using Queuedroid to a specified phone number.
+
+    Args:
+        phone_number (str): The recipient's phone number in E.164 format (e.g., +237123456789).
+        message (str): The content to be sent to the specified phone number.
+
+    Returns:
+        bool: True if the message was sent successfully, False otherwise.
+    """
+    try:
+        data = {
+            "content": message,
+            "exchange_id": QUEUEDROID_EXCHANGE_ID,
+            "phone_number": phone_number,
+        }
+        headers = {"Authorization": f"Bearer {QUEUEDROID_API_KEY}"}
+        response = requests.post(
+            QUEUEDROID_API_URL, json=data, headers=headers, timeout=10
+        )
+
+        if response.ok:
+            logger.info("Message sent successfully via Queuedroid.")
+            return True
+        response.raise_for_status()
+        return False
+    except requests.RequestException as exc:
+        logger.exception("Error sending message via Queuedroid: %s", exc)
+        return False
+
+
+def get_phonenumber_region_code(phone_number: str) -> str:
+    """
+    Get the region code for a given phone number.
+
+    Args:
+        phone_number (str): The phone number in E.164 format.
+
+    Returns:
+        str: The ISO 3166-1 alpha-2 region code corresponding to the phone number.
+    """
+    parsed_number = phonenumbers.parse(phone_number)
+    return geocoder.region_code_for_number(parsed_number)
